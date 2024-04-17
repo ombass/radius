e14f5dd3c24bc4873eb1f085b079f947832ec45e
ombas <darkombas@yandex.ru>
2024-04-17 19:21:14 +0300

    костяк собрал

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Word")
+	//fmt.Println("Hello Word")
 
+	var (
+		login    string
+		password string
+		key      string
+		baza     [2]string
+		status   bool
+	)
+	baza[0] = "admin:admin"
+	baza[1] = "root:root"
+
+	fmt.Print("Login:")
+	fmt.Scanln(&login)
+	fmt.Print("password:")
+	fmt.Scan(&password)
+	key = (login + ":" + password)
+	for i := 0; i < len(baza); i++ {
+		if key == baza[i] {
+			status = true
+		}
+	}
+	if status == true {
+		println("вошёл")
+	} else {
+		println("неверный логин или пароль")
+	}
 }
